92f91d40c6196f4374a853bf13432a69eb2ed5cf
Mandy Cheang <mandycheang135@gmail.com>
2019-11-07 13:11:08 -0500

    update and refactor - display games rules for user experience - allow for case insensitive inputs - display total score and matches won - restrict play again with only 'y' or 'Y' input - refactor match ended logic into smaller methods

--- backend/RB101-RB109/lesson-2/rps_bonus.rb
+++ backend/RB101-RB109/lesson-2/rps_bonus.rb
@@ -36,6 +36,11 @@ def reset_score_board(score_board)
   score_board[:player] = 0
 end
 
+def display_score_board(score_board)
+  "Your score: #{score_board[:player]}.
+  Computer score: #{score_board[:computer]}."
+end
+
 def display_results(player, computer)
   if win?(player, computer)
     "You won!"
@@ -46,24 +51,60 @@ def display_results(player, computer)
   end
 end
 
+def match_ended?(score_board)
+  score_board[:player] == WIN_SCORE || score_board[:computer] == WIN_SCORE
+end
+
+def display_match_winner(score_board)
+  if score_board[:player] == WIN_SCORE
+    "Congrats! You won #{WIN_SCORE} matches, You are the winner!"
+  elsif score_board[:computer] == WIN_SCORE
+    "Congrats! Computer won #{WIN_SCORE} matches, Computer is the winner!"
+  end
+end
+
+def display_choice_options
+  "Choose one:
+  'r'  for rock,
+  'p'  for paper,
+  'sc' for scissors,
+  'sp' for spock,
+  'l'  for lizard"
+end
+
+def display_rules
+  <<-INTRO
+  Welcome to the game of Rock, Paper Scissors, Spock, Lizard!
+  The rules are:
+  Rock crushes Lizard and crushes Scissors.
+  Paper covers Rock and disproves Spock.
+  Scissors cut Paper and decapitates Lizard.
+  Spock smashes Scissors and vaporizes Rock.
+  Lizard poisons Spock and eats Paper.
+
+  You must win #{WIN_SCORE} matches to win the entire game.
+  INTRO
+end
+
+def clear_screen
+  system('clear')
+end
+
+prompt(display_rules)
+
 loop do
   loop do
     choice = ''
     loop do
-      prompt(
-        "Choose one:
-        'r'  for rock,
-        'p'  for paper,
-        'sc' for scissors,
-        'sp' for spock,
-        'l'  for lizard"
-      )
-      choice = gets.chomp
+      prompt(display_choice_options)
+      choice = gets.chomp.downcase
 
       if VALID_CHOICES.include?(choice)
         break
       else
         prompt("That's not a valid choice.")
+        sleep(2)
+        clear_screen
       end
     end
 
@@ -75,26 +116,24 @@ loop do
 
     update_score_board(choice, computer_choice, score_board)
 
-    if score_board[:player] == WIN_SCORE
-      prompt(
-        "Congratulations!
-        You are the grand winner of #{WIN_SCORE} rounds!"
-      )
-      reset_score_board(score_board)
-      break
-    elsif score_board[:computer] == WIN_SCORE
-      prompt(
-        "Congrautlations!
-        Computer is the grand winner of #{WIN_SCORE} rounds!"
-      )
+    prompt(display_score_board(score_board))
+
+    sleep(2)
+    clear_screen
+
+    if match_ended?(score_board)
+      prompt(display_match_winner(score_board))
       reset_score_board(score_board)
       break
     end
   end
 
-  prompt("Do you want to play again?")
+  prompt(
+    "Do you want to play again?
+    Enter Y/y to play again. Enter anything else to exit."
+  )
   answer = gets.chomp
-  break unless answer.downcase().start_with?('y')
+  break unless answer.downcase == 'y'
 end
 
 prompt("Thank you for playing. Goodbye!")
