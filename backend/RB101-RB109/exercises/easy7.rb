a7ea1b8e53f23b87e53fa551e47d84362992a558
Mandy Cheang <mandycheang135@gmail.com>
2019-12-04 15:21:52 -0500

    Easy 7 Question 1

--- backend/RB101-RB109/exercises/easy7.rb
+++ backend/RB101-RB109/exercises/easy7.rb
@@ -1,13 +1,35 @@
 =begin
 1. Combine Two Lists
 =====================
-Input: 
-Output:
-
-Rules:
+Input: Two Arrays
+Output: One Array, contains elements from both Input Arrays
 
 Algorithm:
+- Set counter = 0
+- Set result = []
+- loop until counter >= Array.size
+- For each iteration, insert one element from array1 and one element from array2 to the new array
 =end
+
+def interleave(arr1, arr2)
+  result = []
+  counter = 0
+
+  loop do 
+    break if counter >= arr1.size
+    result << arr1[counter] << arr2[counter]
+    counter += 1
+  end
+
+  result
+end
+
+# Further Exploration
+def interleave2(arr1,  arr2)
+  arr1.zip(arr2).flatten
+end
+
+# interleave2([1, 2, 3], ['a', 'b', 'c']) == [1, 'a', 2, 'b', 3, 'c']
 
 =begin
 2. Lettercase Counter
