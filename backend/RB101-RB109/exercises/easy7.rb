dc8a48888f74b53849cfe5e58749cbc145d7e9d4
Mandy Cheang <mandycheang135@gmail.com>
2019-12-04 14:44:00 -0500

    Template for Easy 7

---
+++ backend/RB101-RB109/exercises/easy7.rb
@@ -0,0 +1,109 @@
+=begin
+1. Combine Two Lists
+=====================
+Input: 
+Output:
+
+Rules:
+
+Algorithm:
+=end
+
+=begin
+2. Lettercase Counter
+=====================
+Input: 
+Output:
+
+Rules:
+
+Algorithm:
+=end
+
+=begin
+3. Caitalize Words
+=====================
+Input: 
+Output:
+
+Rules:
+
+Algorithm:
+=end
+
+=begin
+4. Swap Case
+=====================
+Input: 
+Output:
+
+Rules:
+
+Algorithm:
+=end
+
+=begin
+5. Staggered Caps(Part 1)
+=====================
+Input: 
+Output:
+
+Rules:
+
+Algorithm:
+=end
+
+=begin
+6. Staggered Caps(Part 2)
+=====================
+Input: 
+Output:
+
+Rules:
+
+Algorithm:
+=end
+
+=begin
+7. Multiplicative Average
+=====================
+Input: 
+Output:
+
+Rules:
+
+Algorithm:
+=end
+
+=begin
+8. Multiply Lists
+=====================
+Input: 
+Output:
+
+Rules:
+
+Algorithm:
+=end
+
+=begin
+9. Multiply All Piars
+=====================
+Input: 
+Output:
+
+Rules:
+
+Algorithm:
+=end
+
+=begin
+10. The End is Near But Not Here
+================================
+Input: 
+Output:
+
+Rules:
+
+Algorithm:
+=end
