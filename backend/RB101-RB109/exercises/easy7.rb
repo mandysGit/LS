542fa5fd80ef9d05d754278c5de652e4d650b169
Mandy Cheang <mandycheang135@gmail.com>
2019-12-05 09:34:09 -0500

    Easy 7 Question 2

--- backend/RB101-RB109/exercises/easy7.rb
+++ backend/RB101-RB109/exercises/easy7.rb
@@ -34,13 +34,57 @@ end
 =begin
 2. Lettercase Counter
 =====================
-Input: 
-Output:
-
-Rules:
+Input: String
+Output: Hash - a count of lowercase, uppercase, or neither 
 
 Algorithm:
+- SET constant for lowercase ASCII range
+- SET constant for upper case ASCII range
+- SET hash with 3 keys: lowercase, uppercase, neither
+
+1. Iterate through each characte of the Input String
+2. Use String#ord to get teh ASCII value of the character
+3. Compare the returned value from String#ord to the ASCII ranges defined
+4. Update appropriate Hash key value pair depending on what range the character lies in
+5. return hash
 =end
+UPPERCASE = 'A'.ord..'Z'.ord
+LOWERCASE = 'a'.ord..'z'.ord
+
+def letter_case_count(str)
+  hash = { lowercase: 0, uppercase: 0, neither: 0 }
+
+  str.chars.each do |char|
+    case char.ord
+    when LOWERCASE
+      hash[:lowercase] += 1
+    when UPPERCASE
+      hash[:uppercase] += 1
+    else
+      hash[:neither] += 1
+    end
+  end
+
+  hash
+end
+
+def letter_case_count2(str)
+  str.chars.each_with_object({ lowercase: 0, uppercase: 0, neither: 0 }) do |char, hash|
+    case char.ord
+    when LOWERCASE
+      hash[:lowercase] += 1
+    when UPPERCASE
+      hash[:uppercase] += 1
+    else
+      hash[:neither] += 1
+    end
+  end
+end
+
+# p letter_case_count2('abCdef 123') == { lowercase: 5, uppercase: 1, neither: 4 }
+# p letter_case_count2('AbCd +Ef') == { lowercase: 3, uppercase: 3, neither: 2 }
+# p letter_case_count2('123') == { lowercase: 0, uppercase: 0, neither: 3 }
+# p letter_case_count2('') == { lowercase: 0, uppercase: 0, neither: 0 }
 
 =begin
 3. Caitalize Words
