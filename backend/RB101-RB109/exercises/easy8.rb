3c477f70a28a4e9411d844bd27e2c4b26870721b
Mandy Cheang <mandycheang135@gmail.com>
2019-12-10 12:18:18 -0500

    Easy 8 template

---
+++ backend/RB101-RB109/exercises/easy8.rb
@@ -0,0 +1,109 @@
+=begin
+1. Sum of Sums
+===============
+Input: 
+Output: 
+
+Rules:
+
+Algorithm:
+=end
+
+=begin
+2. Madlibs
+===========
+Input: 
+Output: 
+
+Rules:
+
+Algorithm:
+=end
+
+=begin
+3. Leading Substrings
+======================
+Input: 
+Output: 
+
+Rules:
+
+Algorithm:
+=end
+
+=begin
+4. All Substrings
+==================
+Input: 
+Output: 
+
+Rules:
+
+Algorithm:
+=end
+
+=begin
+5. Palindromic Substrings
+==========================
+Input: 
+Output: 
+
+Rules:
+
+Algorithm:
+=end
+
+=begin
+6. fizzbuzz
+============
+Input: 
+Output: 
+
+Rules:
+
+Algorithm:
+=end
+
+=begin
+7. Double Char (Part 1)
+========================
+Input: 
+Output: 
+
+Rules:
+
+Algorithm:
+=end
+
+=begin
+8. Double Char (Part 2)
+========================
+Input: 
+Output: 
+
+Rules:
+
+Algorithm:
+=end
+
+=begin
+9. Convert number to reversed array of digits
+=============================================
+Input: 
+Output: 
+
+Rules:
+
+Algorithm:
+=end
+
+=begin
+1. Get the Middle Character
+============================
+Input: 
+Output: 
+
+Rules:
+
+Algorithm:
+=end
