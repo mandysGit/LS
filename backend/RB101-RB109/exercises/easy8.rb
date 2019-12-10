d9f90e0292e57820c3e197741f1fca4e3e09c692
Mandy Cheang <mandycheang135@gmail.com>
2019-12-10 13:18:25 -0500

    Easy 8 Question 1

--- backend/RB101-RB109/exercises/easy8.rb
+++ backend/RB101-RB109/exercises/easy8.rb
@@ -1,13 +1,37 @@
 =begin
 1. Sum of Sums
 ===============
-Input: 
-Output: 
+Input: An Array of Numbers
+Output: Integer, total sum of each leading sequence
 
-Rules:
+Rules: Array always contains at least one number
 
 Algorithm:
+1. SET sum = 0
+2. SET counter = 0
+3. ITERATE through input Array
+4. break loop if counter >= length of Array
+5. Use Array#slice to get the range of numbers
+6. Use Array#sum to get the sum of the range of numbers in Array, and reset sum as the old sum plus new sum
+7. return sum
 =end
+
+def sum_of_sums(arr)
+  sum = 0
+  counter = 1
+  
+  until counter > arr.size
+    sum += arr.slice(0, counter).sum
+    counter += 1
+  end
+
+  sum
+end
+
+# p sum_of_sums([3, 5, 2]) == (3) + (3 + 5) + (3 + 5 + 2) # -> (21)
+# p sum_of_sums([1, 5, 7, 3]) == (1) + (1 + 5) + (1 + 5 + 7) + (1 + 5 + 7 + 3) # -> (36)
+# p sum_of_sums([4]) == 4
+# p sum_of_sums([1, 2, 3, 4, 5]) == 35
 
 =begin
 2. Madlibs
