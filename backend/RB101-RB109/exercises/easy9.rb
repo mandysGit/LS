543d6da720cca509206524c7bb6546f73428d053
Mandy Cheang <mandycheang135@gmail.com>
2019-12-13 07:32:59 -0500

    Easy 9 template

---
+++ backend/RB101-RB109/exercises/easy9.rb
@@ -0,0 +1,109 @@
+=begin
+1. Welcome Stranger
+============================
+Input:
+Output:
+
+Rules:
+
+Algorithm:
+=end
+
+=begin
+2. Double Doubles
+============================
+Input:
+Output:
+
+Rules:
+
+Algorithm:
+=end
+
+=begin
+3. Always Return Negative
+============================
+Input:
+Output:
+
+Rules:
+
+Algorithm:
+=end
+
+=begin
+4. Counting Up
+============================
+Input:
+Output:
+
+Rules:
+
+Algorithm:
+=end
+
+=begin
+5. Uppercase Check
+============================
+Input:
+Output:
+
+Rules:
+
+Algorithm:
+=end
+
+=begin
+6. How long are you?s
+============================
+Input:
+Output:
+
+Rules:
+
+Algorithm:
+=end
+
+=begin
+7. Name Swapping
+============================
+Input:
+Output:
+
+Rules:
+
+Algorithm:
+=end
+
+=begin
+8. Sequence Count
+============================
+Input:
+Output:
+
+Rules:
+
+Algorithm:
+=end
+
+=begin
+9. Grade book
+============================
+Input:
+Output:
+
+Rules:
+
+Algorithm:
+=end
+
+=begin
+10. Grocery List
+============================
+Input:
+Output:
+
+Rules:
+
+Algorithm:
+=end
