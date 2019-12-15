2449c1e8245dfe06f58c65dac9df84ed68178638
Mandy Cheang <mandycheang135@gmail.com>
2019-12-15 13:48:05 -0500

    Lesson 5 Practice Problem 7

--- backend/RB101-RB109/lesson-5/practice_problems.rb
+++ backend/RB101-RB109/lesson-5/practice_problems.rb
@@ -74,3 +74,15 @@ munsters = {
 # munsters.each do |name, value|
 #   puts "#{name} is #{value["age"]}-year-old #{value["gender"]}."
 # end
+
+# Problem 7
+a = 2
+b = [5, 8]
+arr = [a, b]
+
+arr[0] += 2
+arr[1][0] -= a
+
+arr #=> [4, [3,8]]
+a #=> 2
+b #=> [3,8]
