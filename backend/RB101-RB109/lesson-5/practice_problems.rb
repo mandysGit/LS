717915746fe85e08f0cc7145e2e13d0c6fb0c263
Mandy Cheang <mandycheang135@gmail.com>
2019-12-15 14:09:33 -0500

    Lesson 5 Practice Problem 10

--- backend/RB101-RB109/lesson-5/practice_problems.rb
+++ backend/RB101-RB109/lesson-5/practice_problems.rb
@@ -104,3 +104,14 @@ arr = [['b', 'c', 'a'], [2, 1, 3], ['blue', 'black', 'green']]
 # end
 
 # p result
+
+# Problem 10
+result = [{a: 1}, {b: 2, c: 3}, {d: 4, e: 5, f: 6}].map do |hash|
+  incremented_hash = {}
+  hash.each do |key, value|
+    incremented_hash[key] = value + 1
+  end
+  incremented_hash
+end
+
+# p result
