1326519cb5e7f7e717964041fc59bb65affb5c6d
Mandy Cheang <mandycheang135@gmail.com>
2019-12-14 20:49:34 -0500

    Easy 9 QUestion 6

--- backend/RB101-RB109/exercises/easy9.rb
+++ backend/RB101-RB109/exercises/easy9.rb
@@ -139,13 +139,36 @@ end
 =begin
 6. How long are you?s
 ============================
-Input:
-Output:
+Input: String
+Output: Array, contains every word from the string, with space and word length appended
 
 Rules:
+- words in the string are separated by exactly one space
+- any substring of non-space characters is a word
 
 Algorithm:
+- Conver input string to Array, Use String#split
+- Iterate through Array, Use Array#map
+- Within the map block, each element is word
+  - Initalize word_length variable as word.length
+  - return "#{word} {#word.length}"
 =end
+
+def word_lengths(str)
+  str.split.map { |word| "#{word} #{word.length}"}
+end
+
+# p word_lengths("cow sheep chicken") == ["cow 3", "sheep 5", "chicken 7"]
+
+# p word_lengths("baseball hot dogs and apple pie") ==
+#   ["baseball 8", "hot 3", "dogs 4", "and 3", "apple 5", "pie 3"]
+
+# p word_lengths("It ain't easy, is it?") == ["It 2", "ain't 5", "easy, 5", "is 2", "it? 3"]
+
+# p word_lengths("Supercalifragilisticexpialidocious") ==
+#   ["Supercalifragilisticexpialidocious 34"]
+
+# p word_lengths("") == []
 
 =begin
 7. Name Swapping
