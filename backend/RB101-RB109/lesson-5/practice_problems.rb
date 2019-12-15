2fc7552c61bd5f078dd2c2c6566bba34bb2d640c
Mandy Cheang <mandycheang135@gmail.com>
2019-12-14 21:29:42 -0500

    Lesson 5 practice problems 2

--- backend/RB101-RB109/lesson-5/practice_problems.rb
+++ backend/RB101-RB109/lesson-5/practice_problems.rb
@@ -1,3 +1,13 @@
 # Problem 1
 arr = ['10', '11', '9', '7', '8']
-p arr.sort { |a, b| b.to_i <=> a.to_i }
+# p arr.sort { |a, b| b.to_i <=> a.to_i }
+
+# Problem 2
+books = [
+  {title: 'One Hundred Years of Solitude', author: 'Gabriel Garcia Marquez', published: '1967'},
+  {title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', published: '1925'},
+  {title: 'War and Peace', author: 'Leo Tolstoy', published: '1869'},
+  {title: 'Ulysses', author: 'James Joyce', published: '1922'}
+]
+
+# p books.sort_by { |hash| hash[:published] }
