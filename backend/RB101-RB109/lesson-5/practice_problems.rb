df9371059de4c7e397ac8e4eee5c6f5843809e36
Mandy Cheang <mandycheang135@gmail.com>
2019-12-15 13:29:16 -0500

    Lesson 5 Practice Problem 6

--- backend/RB101-RB109/lesson-5/practice_problems.rb
+++ backend/RB101-RB109/lesson-5/practice_problems.rb
@@ -61,3 +61,16 @@ munsters = {
 # end
 
 # p total
+
+# Problem 6
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+
+# munsters.each do |name, value|
+#   puts "#{name} is #{value["age"]}-year-old #{value["gender"]}."
+# end
