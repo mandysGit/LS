bf15bd6ebd3bef8358af42ac1f6e9f2dbd1bb002
Mandy Cheang <mandycheang135@gmail.com>
2019-12-16 16:49:00 -0500

    Medium 1 template

---
+++ backend/RB101-RB109/exercises/medium1.rb
@@ -0,0 +1,109 @@
+=begin
+1. Rotation (Part 1)
+====================
+Input: 
+Output: 
+
+Rules: 
+
+Algorithm:
+=end
+
+=begin
+2. Rotation (Part 2)
+====================
+Input: 
+Output: 
+
+Rules: 
+
+Algorithm:
+=end
+
+=begin
+3. Rotation (Part 3)
+====================
+Input: 
+Output: 
+
+Rules: 
+
+Algorithm:
+=end
+
+=begin
+4. 1000 Lights
+==============
+Input: 
+Output: 
+
+Rules: 
+
+Algorithm:
+=end
+
+=begin
+5. Diamonds!
+============
+Input: 
+Output: 
+
+Rules: 
+
+Algorithm:
+=end
+
+=begin
+6. Stack Machine Interpretation
+===============================
+Input: 
+Output: 
+
+Rules: 
+
+Algorithm:
+=end
+
+=begin
+7. Word to Digit
+================
+Input: 
+Output: 
+
+Rules: 
+
+Algorithm:
+=end
+
+=begin
+8. Fibonacci Numbers (Recursion)
+===============================
+Input: 
+Output: 
+
+Rules: 
+
+Algorithm:
+=end
+
+=begin
+9. Fibonacci Numbers (Procedural)
+=================================
+Input: 
+Output: 
+
+Rules: 
+
+Algorithm:
+=end
+
+=begin
+10. Fibonacci Numbers (Last Digit)
+==================================
+Input: 
+Output: 
+
+Rules: 
+
+Algorithm:
+=end
