7971953c9d7b73c0cc2b2771cbbd4425c546d030
Mandy Cheang <mandycheang135@gmail.com>
2019-12-16 17:03:02 -0500

    Medium 1 Question 1

--- backend/RB101-RB109/exercises/medium1.rb
+++ backend/RB101-RB109/exercises/medium1.rb
@@ -1,13 +1,45 @@
 =begin
 1. Rotation (Part 1)
 ====================
-Input: 
-Output: 
+Input: Array 
+Output: New Array, where first element is moved to the end of Array
 
 Rules: 
+- Dont' use Array#rotate
+- don't modify the original Array
 
 Algorithm:
+- Object#dup to create shallow copy of input Array and save it as result
+- initalize first_elm as first element of result, use Array#shift 
+- Use Array#push to insert the first_elm in last position
 =end
+
+def rotate_array(arr)
+  result = arr.dup
+  first_elm = result.shift
+  result << first_elm
+
+  result
+end
+
+# Further Exploration
+def rotate_string(str)
+  rotate_array(str.chars).join
+end
+
+def rotate_integer(int)
+  rotate_array(int.to_s.chars).join.to_i
+end
+
+# p rotate_string('hello')
+# p rotate_integer(1234)
+# p rotate_array([7, 3, 5, 2, 9, 1]) == [3, 5, 2, 9, 1, 7]
+# p rotate_array(['a', 'b', 'c']) == ['b', 'c', 'a']
+# p rotate_array(['a']) == ['a']
+
+# x = [1, 2, 3, 4]
+# p rotate_array(x) == [2, 3, 4, 1]   # => true
+# x == [1, 2, 3, 4]                 # => true
 
 =begin
 2. Rotation (Part 2)
