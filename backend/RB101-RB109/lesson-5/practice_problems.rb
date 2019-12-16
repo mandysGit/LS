0009dbfa38f23462fcc6be9b55d8b793f066050e
Mandy Cheang <mandycheang135@gmail.com>
2019-12-16 08:56:19 -0500

    Lesson 5 Practice Problem 12

--- backend/RB101-RB109/lesson-5/practice_problems.rb
+++ backend/RB101-RB109/lesson-5/practice_problems.rb
@@ -124,3 +124,12 @@ result = arr.map do |sub_array|
 end
 
 # p result
+
+# Problem 12
+arr = [[:a, 1], ['b', 'two'], ['sea', {c: 3}], [{a: 1, b: 2, c: 3, d: 4}, 'D']]
+
+hsh = arr.each_with_object({}) do |sub_array, hash|
+  hash[sub_array[0]] = sub_array[1]
+end
+
+# p hsh
