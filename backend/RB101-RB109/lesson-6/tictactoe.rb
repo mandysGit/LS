b5b98353982db558411223f0c83269987a5add04
Mandy Cheang <mandycheang135@gmail.com>
2019-12-17 16:20:00 -0500

    Add main game loop for player and computer

--- backend/RB101-RB109/lesson-6/tictactoe.rb
+++ backend/RB101-RB109/lesson-6/tictactoe.rb
@@ -1,11 +1,15 @@
+require 'pry'
+
 INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
 COMPUTER_MARKER = 'O'
+
 def prompt(msg)
   puts "=> #{msg}"
 end
 
 def display_board(brd)
+  system 'clear'
   puts ""
   puts "     |     |"
   puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
@@ -43,10 +47,27 @@ def player_places_piece!(brd)
   brd[square] = PLAYER_MARKER
 end
 
+def computer_places_piece!(brd)
+  square = emtpy_squares(brd).sample
+  brd[square] = COMPUTER_MARKER
+end
+
+def board_full?(brd)
+  emtpy_squares(brd).empty?
+end
+
+def someone_won?(brd)
+  false
+end
 board = initialize_board
 
 display_board(board)
 
-player_places_piece!(board)
-puts board.inspect
+loop do
+  player_places_piece!(board)
+  computer_places_piece!(board)
+  display_board(board)
+  break if someone_won?(board) || board_full?(board)
+end 
+
 display_board(board)
