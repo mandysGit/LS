139018ddb0fbd48a1f1c84c4ed6f36ae580568b1
Mandy Cheang <mandycheang135@gmail.com>
2019-12-19 17:45:13 -0500

    Medium 1 Question 8

--- backend/RB101-RB109/exercises/medium1.rb
+++ backend/RB101-RB109/exercises/medium1.rb
@@ -460,14 +460,31 @@ end
 =begin
 8. Fibonacci Numbers (Recursion)
 ===============================
-Input:
-Output:
+Input: Integer, Fib sequence
+Output: Integer, Fib number 
 
 Rules:
+F(1) = 1
+F(2) = 1
+F(n) = F(n - 1) + F(n - 2) where n > 2
 
 Algorithm:
 =end
 
+def fibonacci(n)
+  return 1 if n == 1
+  return 1 if n == 2
+
+  return fibonacci(n - 1) + fibonacci(n - 2)
+end
+
+# p fibonacci(1) == 1
+# p fibonacci(2) == 1
+# p fibonacci(3) == 2
+# p fibonacci(4) == 3
+# p fibonacci(5) == 5
+# p fibonacci(12) == 144
+# p fibonacci(20) == 6765
 =begin
 9. Fibonacci Numbers (Procedural)
 =================================
