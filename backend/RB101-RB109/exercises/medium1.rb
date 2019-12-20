7edc64b124d5d2e41edf4c664e8435fc761ba850
Mandy Cheang <mandycheang135@gmail.com>
2019-12-20 17:09:09 -0500

    Medium 1 Question 10

--- backend/RB101-RB109/exercises/medium1.rb
+++ backend/RB101-RB109/exercises/medium1.rb
@@ -528,10 +528,15 @@ end
 =begin
 10. Fibonacci Numbers (Last Digit)
 ==================================
-Input:
-Output:
-
-Rules:
-
-Algorithm:
 =end
+
+def fibonacci_last(n)
+  fibonacci(n) % 10
+end
+
+# p fibonacci_last(15)        # -> 0  (the 15th Fibonacci number is 610)
+# p fibonacci_last(20)        # -> 5 (the 20th Fibonacci number is 6765)
+# p fibonacci_last(100)       # -> 5 (the 100th Fibonacci number is 354224848179261915075)
+# p fibonacci_last(100_001)   # -> 1 (this is a 20899 digit number)
+# p fibonacci_last(1_000_007) # -> 3 (this is a 208989 digit number)
+# p fibonacci_last(123456789) # -> 4
