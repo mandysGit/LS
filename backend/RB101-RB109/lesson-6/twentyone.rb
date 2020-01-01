6254ca979fa5a13e6bc765b07d6e700f0e8d65e6
Mandy Cheang <mandycheang135@gmail.com>
2020-01-01 18:25:14 -0500

    Deal initial cards and display the cards

--- backend/RB101-RB109/lesson-6/twentyone.rb
+++ backend/RB101-RB109/lesson-6/twentyone.rb
@@ -1,11 +1,39 @@
-deck = { :hearts   => [2, 3, 4, 5, 6, 7, 8, 9, 10, :jack, :queen, :king, :ace],
-         :diamonds => [2, 3, 4, 5, 6, 7, 8, 9, 10, :jack, :queen, :king, :ace],
-         :clubs    => [2, 3, 4, 5, 6, 7, 8, 9, 10, :jack, :queen, :king, :ace],
-         :spades   => [2, 3, 4, 5, 6, 7, 8, 9, 10, :jack, :queen, :king, :ace] }
+deck = { hearts: ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king', 'ace'],
+         diamonds: ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king', 'ace'],
+         clubs: ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king', 'ace'],
+         spades: ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king', 'ace'] 
+        }
 
+player = []
+dealer = []
+
+def prompt(msg)
+  puts "=> #{msg}"
+end
 def shuffle!(deck)
   deck.each_pair do |_, cards|
     cards.shuffle!
   end
   deck
 end
+
+def deal_card!(deck, current_player)
+  shuffle!(deck)
+  suit = deck.keys.sample
+  current_player << deck[suit].pop
+end
+
+def display_cards(player, dealer)
+  "Dealer has: #{dealer.first} and unknown card
+  You have: #{player[0]} and #{player[1]}"
+end
+
+2.times do 
+  deal_card!(deck, player)
+  deal_card!(deck, dealer)
+end
+
+# pp deck
+# p player
+# p dealer
+prompt display_cards(player, dealer)
