f37db5208cbd9aef2c6e15d092f3535f829a7512
Mandy Cheang <mandycheang135@gmail.com>
2020-02-07 10:16:55 -0500

    Finished Fix the Problem - Books

--- backend/RB120-RB129/exercises/easy1.rb
+++ backend/RB120-RB129/exercises/easy1.rb
@@ -114,7 +114,49 @@ name += 1 # name is reassigned name = 42 + 1
 
 # String#to_s converts 42 to String '42', therefore
 # upcase is called on the String '42' on the expression @name.upcase
-puts fluffy.name
-puts fluffy
-puts fluffy.name
-puts name
+# puts fluffy.name
+# puts fluffy
+# puts fluffy.name
+# puts name
+
+
+# 3. Fix the Program - Books (Part 1)
+# class Book
+#   attr_reader :title, :author
+
+#   def initialize(author, title)
+#     @author = author
+#     @title = title
+#   end
+
+#   def to_s
+#     %("#{title}", by #{author})
+#   end
+# end
+
+# book = Book.new("Neil Stephenson", "Snow Crash")
+# puts %(The author of "#{book.title}" is #{book.author}.)
+# puts %(book = #{book}.)
+
+=begin Further Exploration
+We use attr_reader instead of attr_writer or attr_accessor is because
+we only wnat to retrieve the title and author, we don't want to reassign it. 
+Therefore, we only need a getter and not a setting method, attr_reader gives us
+a getter method to retrieve @author and @title instance variables.
+=end
+
+
+# 4. Fix the Program - Books (Part 2)
+class Book
+  attr_accessor :title, :author
+
+  def to_s
+    %("#{title}", by #{author})
+  end
+end
+
+book = Book.new
+book.author = "Neil Stephenson"
+book.title = "Snow Crash"
+puts %(The author of "#{book.title}" is #{book.author}.)
+puts %(book = #{book}.)
