5683188f8fdc28a65acb81d8ac6668edd8e2ea9d
Mandy Cheang <mandycheang135@gmail.com>
2020-02-11 16:16:37 -0500

    Add class implementation to rps

--- backend/RB120-RB129/lesson-2/rps.rb
+++ backend/RB120-RB129/lesson-2/rps.rb
@@ -1,44 +1,136 @@
+class Move
+  VALUES = ['rock', 'paper', 'scissors']
+
+  def initialize(value)
+    @value = value
+  end
+
+  def scissors?
+    @value == 'scissors'
+  end
+
+  def rock?
+    @value == 'rock'
+  end
+
+  def paper?
+    @value == 'paper'
+  end
+
+  def to_s
+    @value
+  end
+
+  def >(other_move)
+    (rock? && other_move.scissors?) ||
+      (paper? && other_move.rock?) ||
+      (scissors? && other_move.paper?)
+  end
+
+  def <(other_move)
+    (rock? && other_move.paper?) ||
+      (paper? && other_move.scissors?) ||
+      (scissors? && other_move.rock?)
+  end
+end
+
 class Player
+  attr_accessor :move, :name
+
   def initialize
-    # maybe a "name"? what about a "move"?
+    set_name
+  end
+end
+
+class Human < Player
+  def set_name
+    n = ''
+    loop do
+      puts "Whats your name?"
+      n = gets.chomp
+      break unless n.empty?
+      puts "Sorry, must enter a value."
+    end
+    self.name = n
   end
 
   def choose
-
+    choice = nil
+    loop do
+      puts "Please choose rock, paper, or scissors: "
+      choice = gets.chomp
+      break if Move::VALUES.include? choice
+      puts "Sorry, invalid choice."
+    end
+    self.move = Move.new(choice)
   end
 end
 
-class Move
-  def initialize
-    # seems like we need something to keep track
-    # of the choice... a move object can be "paper", "rock" or "scissors"
+class Computer < Player
+  def set_name
+    self.name = ['R2D2', 'Hal', 'Chappie', 'Sonny', 'Number 5'].sample
   end
-end
 
-class Rule
-  def initialize
-    # not sure what the "state" of a rule object should be
+  def choose
+    self.move = Move.new(Move::VALUES.sample)
   end
-end
-
-# not sure where "compare" goes yet
-def compare(move1, move2)
-
 end
 
 class RPSGame
-  attr_accesor :human, :computer
+  attr_accessor :human, :computer
 
   def initialize
-    @human = Player.new
-    @computer = Player.new
+    @human = Human.new
+    @computer = Computer.new
+  end
+
+  def display_welcome_message
+    puts "Welcome to Rock, Paper, Scissors!"
+  end
+
+  def display_goodbye_message
+    puts "Thanks for playing Rock, Paper, Scissors. Good bye!"
+  end
+
+  def display_moves
+    puts "#{human.name} chose #{human.move}."
+    puts "#{computer.name} chose move #{computer.move}."
+  end
+
+  def display_winner
+    if human.move > computer.move
+      puts "#{human.name} won!"
+    elsif human.move < computer.move
+      puts "#{computer.name} won!"
+    else
+      puts "It's a tie!"
+    end
+  end
+
+  def play_again?
+    answer = nil
+    loop do
+      puts "Would you like to play again? (y/n)"
+      answer = gets.chomp
+      break if ['y', 'n'].include? answer.downcase
+      puts "Sorry, must be y or n"
+    end
+
+    return true if answer.downcase == 'y'
+    return false if answer.downcase == 'n'
   end
 
   def play
     display_welcome_message
-    human_choose_move
-    computer_choose_move
-    display_winner
+    loop do
+      human.choose
+      computer.choose
+      display_moves
+      display_winner
+      break unless play_again?
+    end
     display_goodbye_message
   end
 end
+
+RPSGame.new.play
