349ca3536186b05f926cf23ae648692e4cef286b
Mandy Cheang <mandycheang135@gmail.com>
2020-03-24 15:09:10 -0400

    Add welcome and goodbye messages, display board

--- backend/RB120-RB129/lesson-5/tictactoe.rb
+++ backend/RB120-RB129/lesson-5/tictactoe.rb
@@ -1,3 +1,14 @@
+module Promptable
+  def prompt(msg)
+    puts "=> #{msg}"
+  end
+
+  def display(msg)
+    puts "
+    #{msg}"
+  end
+end
+
 class Board
   def initialize
   end
@@ -17,18 +28,52 @@ class Player
 end
 
 class TTTGame
+  include Promptable
+
   def play
     display_welcome_message
+
     loop do
       display_board
+      break
       first_player_moves
       break if someone_won? || board_full?
 
       second_player_moves
       break if someone_won? || board_full?
     end
-    display_result
+
+    # display_result
     display_goodbye_message
+  end
+
+  private
+
+
+  def display_welcome_message
+    display("Welcome to Tic Tac Toe.
+    The rules to win a round is to mark 3 squares
+    in a row with the same marker.")
+  end
+
+  def display_goodbye_message
+    display("\u{1F600} Thank You for playing Rock, Paper, Scissors! Good bye!")
+  end
+
+  def display_board
+    puts ""
+    puts "     |     |"
+    puts "     |     |   "
+    puts "     |     |"
+    puts "-----+-----+-----"
+    puts "     |     |"
+    puts "     |     |   "
+    puts "     |     |"
+    puts "-----+-----+-----"
+    puts "     |     |"
+    puts "     |     |   "
+    puts "     |     |"
+    puts ""
   end
 end
 
