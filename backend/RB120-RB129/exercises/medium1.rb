fa48a329842d16dbe02d4f94e12a980d46680e80
Mandy Cheang <mandycheang135@gmail.com>
2020-03-29 21:07:19 -0400

    RB120 exercise Medium 1, Quesiton 1

---
+++ backend/RB120-RB129/exercises/medium1.rb
@@ -0,0 +1,42 @@
+
+#1)
+class Machine
+  def start
+    flip_switch(:on)
+  end
+
+  def stop
+    flip_switch(:off)
+  end
+
+  private
+
+  attr_writer :switch
+
+  def flip_switch(desired_state)
+    self.switch = desired_state
+  end
+end
+
+# Further Exploration
+class Machine
+  def start
+    flip_switch(:on)
+  end
+
+  def stop
+    flip_switch(:off)
+  end
+
+  def display_switch
+    puts switch
+  end
+
+  private
+
+  attr_accessor :switch
+
+  def flip_switch(desired_state)
+    self.switch = desired_state
+  end
+end
