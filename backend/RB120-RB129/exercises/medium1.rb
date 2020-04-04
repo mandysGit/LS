722bfa82769df521d0aff72dd3e244916cd04a37
Mandy Cheang <mandycheang135@gmail.com>
2020-04-03 22:08:44 -0400

    RB120 Medium 1 Question 4

--- backend/RB120-RB129/exercises/medium1.rb
+++ backend/RB120-RB129/exercises/medium1.rb
@@ -132,3 +132,63 @@ end
 
 # alice = Undergraduate.new('Alice', '2020')
 # p alice
+
+#4) Circular Queue
+class CircularQueue
+  def initialize(size)
+    @queue = []
+    @size = size
+  end
+
+  def enqueue(value)
+    if @queue.size < @size
+      @queue << value
+    else
+      @queue.shift
+      @queue << value
+    end
+  end
+
+  def dequeue
+    @queue.shift
+  end
+end
+
+queue = CircularQueue.new(3)
+puts queue.dequeue == nil
+
+queue.enqueue(1)
+queue.enqueue(2)
+puts queue.dequeue == 1
+
+queue.enqueue(3)
+queue.enqueue(4)
+puts queue.dequeue == 2
+
+queue.enqueue(5)
+queue.enqueue(6)
+queue.enqueue(7)
+puts queue.dequeue == 5
+puts queue.dequeue == 6
+puts queue.dequeue == 7
+puts queue.dequeue == nil
+
+queue = CircularQueue.new(4)
+puts queue.dequeue == nil
+
+queue.enqueue(1)
+queue.enqueue(2)
+puts queue.dequeue == 1
+
+queue.enqueue(3)
+queue.enqueue(4)
+puts queue.dequeue == 2
+
+queue.enqueue(5)
+queue.enqueue(6)
+queue.enqueue(7)
+puts queue.dequeue == 4
+puts queue.dequeue == 5
+puts queue.dequeue == 6
+puts queue.dequeue == 7
+puts queue.dequeue == nil
