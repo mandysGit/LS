3b0bc63adf6d0808a69254f560db01fe0c242981
Mandy Cheang <mandycheang135@gmail.com>
2020-04-04 21:52:26 -0400

    RB120 exercises Medium Question 6

--- backend/RB120-RB129/exercises/medium1.rb
+++ backend/RB120-RB129/exercises/medium1.rb
@@ -154,41 +154,107 @@ class CircularQueue
   end
 end
 
-queue = CircularQueue.new(3)
-puts queue.dequeue == nil
+# queue = CircularQueue.new(3)
+# puts queue.dequeue == nil
 
-queue.enqueue(1)
-queue.enqueue(2)
-puts queue.dequeue == 1
+# queue.enqueue(1)
+# queue.enqueue(2)
+# puts queue.dequeue == 1
 
-queue.enqueue(3)
-queue.enqueue(4)
-puts queue.dequeue == 2
+# queue.enqueue(3)
+# queue.enqueue(4)
+# puts queue.dequeue == 2
 
-queue.enqueue(5)
-queue.enqueue(6)
-queue.enqueue(7)
-puts queue.dequeue == 5
-puts queue.dequeue == 6
-puts queue.dequeue == 7
-puts queue.dequeue == nil
+# queue.enqueue(5)
+# queue.enqueue(6)
+# queue.enqueue(7)
+# puts queue.dequeue == 5
+# puts queue.dequeue == 6
+# puts queue.dequeue == 7
+# puts queue.dequeue == nil
 
-queue = CircularQueue.new(4)
-puts queue.dequeue == nil
+# queue = CircularQueue.new(4)
+# puts queue.dequeue == nil
 
-queue.enqueue(1)
-queue.enqueue(2)
-puts queue.dequeue == 1
+# queue.enqueue(1)
+# queue.enqueue(2)
+# puts queue.dequeue == 1
 
-queue.enqueue(3)
-queue.enqueue(4)
-puts queue.dequeue == 2
+# queue.enqueue(3)
+# queue.enqueue(4)
+# puts queue.dequeue == 2
 
-queue.enqueue(5)
-queue.enqueue(6)
-queue.enqueue(7)
-puts queue.dequeue == 4
-puts queue.dequeue == 5
-puts queue.dequeue == 6
-puts queue.dequeue == 7
-puts queue.dequeue == nil
+# queue.enqueue(5)
+# queue.enqueue(6)
+# queue.enqueue(7)
+# puts queue.dequeue == 4
+# puts queue.dequeue == 5
+# puts queue.dequeue == 6
+# puts queue.dequeue == 7
+# puts queue.dequeue == nil
+
+
+# 6) Number Guesser Part 1
+class GuessingGame
+  LOWER = 1
+  UPPER = 100
+
+  def initialize
+    @limit = 7
+    @target = Random.new.rand(1..100)
+    @guess = nil
+  end
+
+  def play
+    loop do
+      if no_more_guesses?
+        puts "You have no more guesses. You lost!"
+        break
+      elsif correct_guess?
+        break
+      end
+
+      start
+    end
+  end
+
+  private
+
+  def start
+    puts ""
+    puts "You have #{@limit} guesses remaining."
+
+    loop do
+      print "Enter a number between #{LOWER} and #{UPPER}: "
+      @guess = gets.chomp.to_i
+      break if (1..100).include?(@guess)
+
+      print "Invalid guess."
+    end
+
+    @limit = @limit - 1
+    validate(@guess)
+  end
+
+  def validate(guess)
+    if correct_guess?
+      puts "That's the number!"
+      puts "You won!"
+    elsif guess > @target
+      puts "Your guess is too high."
+    else
+      puts "Your guess is too low."
+    end
+  end
+
+  def no_more_guesses?
+    @limit == 0
+  end
+
+  def correct_guess?
+    @guess == @target
+  end
+end
+
+game = GuessingGame.new
+game.play
