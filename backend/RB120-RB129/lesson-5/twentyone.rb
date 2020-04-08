e3c4e00c95b05990151609bd9a62438dcc47ae1d
Mandy Cheang <mandycheang135@gmail.com>
2020-04-07 20:11:41 -0400

    Create template for OO twentyone

---
+++ backend/RB120-RB129/lesson-5/twentyone.rb
@@ -0,0 +1,59 @@
+class Player
+  def initialize
+  end
+
+  def hit
+  end
+
+  def stay
+  end
+
+  def busted?
+  end
+
+  def total
+  end
+end
+
+class Dealer
+  def initialize
+  end
+
+  def deal
+  end
+
+  def hit
+  end
+
+  def stay
+  end
+
+  def busted?
+  end
+
+  def total
+  end
+end
+
+class Participant
+end
+
+class Deck
+  def initialize
+  end
+
+  def deal
+  end
+end
+
+class Card
+  def initialize
+  end
+end
+
+class Game
+  def start
+  end
+end
+
+Game.new.start
