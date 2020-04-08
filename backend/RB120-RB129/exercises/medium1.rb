35bde922a20926f8285a4bc91924423a19dce071
Mandy Cheang <mandycheang135@gmail.com>
2020-04-08 13:05:13 -0400

    RB120 Medium Question 7

--- backend/RB120-RB129/exercises/medium1.rb
+++ backend/RB120-RB129/exercises/medium1.rb
@@ -195,13 +195,79 @@ end
 
 
 # 6) Number Guesser Part 1
-class GuessingGame
-  LOWER = 1
-  UPPER = 100
+# class GuessingGame
+#   LOWER = 1
+#   UPPER = 100
 
-  def initialize
-    @limit = 7
-    @target = Random.new.rand(1..100)
+#   def initialize
+#     @limit = 7
+#     @target = Random.new.rand(1..100)
+#     @guess = nil
+#   end
+
+#   def play
+#     loop do
+#       if no_more_guesses?
+#         puts "You have no more guesses. You lost!"
+#         break
+#       elsif correct_guess?
+#         break
+#       end
+
+#       start
+#     end
+#   end
+
+#   private
+
+#   def start
+#     puts ""
+#     puts "You have #{@limit} guesses remaining."
+
+#     loop do
+#       print "Enter a number between #{LOWER} and #{UPPER}: "
+#       @guess = gets.chomp.to_i
+#       break if (1..100).include?(@guess)
+
+#       print "Invalid guess."
+#     end
+
+#     @limit = @limit - 1
+#     validate(@guess)
+#   end
+
+#   def validate(guess)
+#     if correct_guess?
+#       puts "That's the number!"
+#       puts "You won!"
+#     elsif guess > @target
+#       puts "Your guess is too high."
+#     else
+#       puts "Your guess is too low."
+#     end
+#   end
+
+#   def no_more_guesses?
+#     @limit == 0
+#   end
+
+#   def correct_guess?
+#     @guess == @target
+#   end
+# end
+
+# game = GuessingGame.new
+# game.play
+
+
+
+# 7) Number Guesser Part 1
+class GuessingGame
+  def initialize(low, high)
+    @limit = Math.log2((low..high).to_a.length).to_i + 1
+    @target = Random.new.rand(low..high)
+    @low = low
+    @high = high
     @guess = nil
   end
 
@@ -225,9 +291,9 @@ class GuessingGame
     puts "You have #{@limit} guesses remaining."
 
     loop do
-      print "Enter a number between #{LOWER} and #{UPPER}: "
+      print "Enter a number between #{@low} and #{@high}: "
       @guess = gets.chomp.to_i
-      break if (1..100).include?(@guess)
+      break if (@low..@high).include?(@guess)
 
       print "Invalid guess."
     end
@@ -256,5 +322,5 @@ class GuessingGame
   end
 end
 
-game = GuessingGame.new
+game = GuessingGame.new(501, 1500)
 game.play
