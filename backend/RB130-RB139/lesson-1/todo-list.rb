095b570e6ddeb978da8859c5f88d46485854fbd1
Mandy Cheang <mandycheang135@gmail.com>
2020-05-01 21:42:15 -0400

    Add methods to TodoList class

--- backend/RB130-RB139/lesson-1/todo-list.rb
+++ backend/RB130-RB139/lesson-1/todo-list.rb
@@ -1,7 +1,3 @@
-# This class represents a todo item and its associated
-# data: name and description. There's also a "done"
-# flag to show whether this todo item is done.
-
 class Todo
   DONE_MARKER = 'X'
   UNDONE_MARKER = ' '
@@ -37,9 +33,7 @@ class Todo
   end
 end
 
-# This class represents a collection of Todo objects.
-# You can perform typical collection-oriented actions
-# on a TodoList object, including iteration and selection.
+
 
 class TodoList
   attr_accessor :title
@@ -49,6 +43,75 @@ class TodoList
     @todos = []
   end
 
-  # rest of class needs implementation
+  def add(todo)
+    if todo.instance_of? Todo
+      todos << todo
+    else
+      raise TypeError, "Can only add Todo objects"
+    end
+  end
 
+  def size
+    todos.size
+  end
+
+  def first
+    todos.first
+  end
+   
+  def last
+    todos.last
+  end
+
+  def to_a
+    todos
+  end
+
+  def done?
+    todos.all? { |todo| todo.done? == true }
+  end
+
+  def item_at(index)
+    todos.fetch(index)
+  end
+
+  private 
+
+  attr_accessor :todos
 end
+
+# given
+todo1 = Todo.new("Buy milk")
+todo2 = Todo.new("Clean room")
+todo3 = Todo.new("Go to gym")
+list = TodoList.new("Today's Todos")
+
+# ---- Adding to the list -----
+
+# add
+list.add(todo1)                 # adds todo1 to end of list, returns list
+list.add(todo2)                 # adds todo2 to end of list, returns list
+list.add(todo3)                 # adds todo3 to end of list, returns list
+# list.add(1)                     # raises TypeError with message "Can only add Todo objects"
+
+# size
+p list.size                       # returns 3
+
+# first
+p list.first                      # returns todo1, which is the first item in the list
+
+# last
+p list.last                       # returns todo3, which is the last item in the list
+
+#to_a
+p list.to_a                      # returns an array of all items in the list
+
+#done?
+p list.done?                     # returns true if all todos in the list are done, otherwise false
+
+# ---- Retrieving an item in the list ----
+
+# item_at
+# list.item_at                    # raises ArgumentError
+# p list.item_at(1)                 # returns 2nd item in list (zero based index)
+ list.item_at(100)               # raises IndexError
