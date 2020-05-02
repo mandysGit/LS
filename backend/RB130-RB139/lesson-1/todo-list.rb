cbbb52916bae24da8889a231e3502c9566fcc028
Mandy Cheang <mandycheang135@gmail.com>
2020-05-02 18:08:59 -0400

    Finished ToDo List class

--- backend/RB130-RB139/lesson-1/todo-list.rb
+++ backend/RB130-RB139/lesson-1/todo-list.rb
@@ -44,12 +44,10 @@ class TodoList
   end
 
   def add(todo)
-    if todo.instance_of? Todo
-      todos << todo
-    else
-      raise TypeError, "Can only add Todo objects"
-    end
+    raise TypeError, 'can only add Todo objects' unless  todo.instance_of? Todo
+    todos << todo
   end
+  alias_method :<<, :add
 
   def size
     todos.size
@@ -64,20 +62,46 @@ class TodoList
   end
 
   def to_a
-    todos
+    todos.clone
   end
 
   def done?
-    todos.all? { |todo| todo.done? == true }
+    todos.all? { |todo| todo.done? }
+  end
+
+  def done!
+    todos.each { |todo| todo.done! }
   end
 
   def item_at(index)
     todos.fetch(index)
   end
+  
+  def mark_done_at(index)
+    item_at(index).done!
+  end
+
+  def shift
+    todos.shift
+  end
+
+  def pop
+    todos.pop
+  end
+
+  def remove_at(index)
+    todos.delete_at(item_at(index))
+  end
+
+  def to_s
+    result = "---- #{title} ----\n" 
+    todos.each { |todo| result << "#{todo}\n" }
+    result
+  end
 
   private 
 
-  attr_accessor :todos
+  attr_reader :todos
 end
 
 # given
@@ -85,33 +109,16 @@ todo1 = Todo.new("Buy milk")
 todo2 = Todo.new("Clean room")
 todo3 = Todo.new("Go to gym")
 list = TodoList.new("Today's Todos")
+list.add(todo1)
+list.add(todo2)
+list.add(todo3)
 
-# ---- Adding to the list -----
+puts list
 
-# add
-list.add(todo1)                 # adds todo1 to end of list, returns list
-list.add(todo2)                 # adds todo2 to end of list, returns list
-list.add(todo3)                 # adds todo3 to end of list, returns list
-# list.add(1)                     # raises TypeError with message "Can only add Todo objects"
+list.pop
 
-# size
-p list.size                       # returns 3
+puts list
 
-# first
-p list.first                      # returns todo1, which is the first item in the list
+list.mark_done_at(1)
 
-# last
-p list.last                       # returns todo3, which is the last item in the list
-
-#to_a
-p list.to_a                      # returns an array of all items in the list
-
-#done?
-p list.done?                     # returns true if all todos in the list are done, otherwise false
-
-# ---- Retrieving an item in the list ----
-
-# item_at
-# list.item_at                    # raises ArgumentError
-# p list.item_at(1)                 # returns 2nd item in list (zero based index)
- list.item_at(100)               # raises IndexError
+puts list
