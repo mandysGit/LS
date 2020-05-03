70672fb88235bfe95e75b306911d01c3279361ef
Mandy Cheang <mandycheang135@gmail.com>
2020-05-03 13:21:55 -0400

    Implement custome TodoList#each

--- backend/RB130-RB139/lesson-1/todo-list.rb
+++ backend/RB130-RB139/lesson-1/todo-list.rb
@@ -99,26 +99,34 @@ class TodoList
     result
   end
 
+  def each
+    counter = 0
+
+    while counter < todos.size
+      yield(todos[counter])
+      counter += 1
+    end
+
+    todos
+  end
+
   private 
 
   attr_reader :todos
 end
 
-# given
+
+
 todo1 = Todo.new("Buy milk")
 todo2 = Todo.new("Clean room")
 todo3 = Todo.new("Go to gym")
+
 list = TodoList.new("Today's Todos")
 list.add(todo1)
 list.add(todo2)
 list.add(todo3)
 
-puts list
+list.each do |todo|
+  puts todo                   # calls Todo#to_s
+end
 
-list.pop
-
-puts list
-
-list.mark_done_at(1)
-
-puts list
