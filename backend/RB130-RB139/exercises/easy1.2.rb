cb4a2eb6993b3adb4cdf74a4aa019b0f11535b91
Mandy Cheang <mandycheang135@gmail.com>
2020-05-05 12:59:01 -0400

    Add Further Exploration for Easy1 Question 2

--- backend/RB130-RB139/exercises/easy1.2.rb
+++ backend/RB130-RB139/exercises/easy1.2.rb
@@ -1,10 +1,21 @@
 # Optional Blocks
 
-def compute
-  return  yield if block_given?
-  return  "Does not compute." if !block_given?
+#def compute
+#  return  yield if block_given?
+#  return  "Does not compute." if !block_given?
+#end
+#
+# compute { 5 + 3 } == 8
+# compute { 'a' + 'b' } == 'ab'
+# compute == 'Does not compute.'
+
+# Further Exploration
+
+def compute(arg)
+  return 'Does not compute.' unless block_given?
+  yield(arg)
 end
 
-p compute { 5 + 3 } == 8
-p compute { 'a' + 'b' } == 'ab'
-p compute == 'Does not compute.'
+p compute(5) { |num| num + 3 } == 8
+p compute('a') { |num| num + 'b' } == 'ab'
+p compute(5) == 'Does not compute.'
