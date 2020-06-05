55d6982b81b2e1ba8f75014c183af1e900c28bdd
Mandy Cheang <mandycheang135@gmail.com>
2020-06-05 14:26:01 -0400

    Refactor Sum of Multiples Easy 5 - ruby challenge

--- backend/RB130-RB139/ruby-challenges/sum_of_multiples.rb
+++ backend/RB130-RB139/ruby-challenges/sum_of_multiples.rb
@@ -8,54 +8,27 @@ Data structure:
 - output: Integer
 
 Algo:
-1) Create an array of all the multiples
-- Create an array of all numbers up and including one less than limit
-- IF set of numbers were given
-  - create helper method `find_multiples`
-  - pass in an array of number set
-  - pass in the limit
-  - use 1.upto(limit - 1)
-  - array#each to loop over the number set
-  - check each number to see if it's a multiple of the base
-
-- IF no set of numbers given
-  - use array#select to select all numbers multiple of 3 and 5
-
-2) Sum all the multiples
+- Create an array of natural numbers up to but not including limit
+- loop over each of the natural numbers
+- accumulate each number to an overall sum IF the number is an multiple of the set
 =end
 
 class SumOfMultiples
-  def initialize(*nums)
-    @nums = nums
+  def initialize(*set)
+    @set = set
   end
 
-  def self.to(limit)
-    natural_nums = (0..limit-1).to_a
-    nums = @nums || [3,5]
-
-    result = natural_nums.inject do |sum, n|
-      if nums.any? { |multiple| n % multiple == 0 }
-        sum + n
+  def self.to(limit, set=[3,5])
+    (0...limit).reduce do |sum, num|
+      if set.any? { |multiple| num % multiple == 0 }
+        sum + num
       else
         sum
       end
     end
-
-    p result
   end
 
   def to(limit)
-    natural_nums = (0..limit-1).to_a
-    nums = @nums || [3,5]
-
-    result = natural_nums.inject do |sum, n|
-      if nums.any? { |multiple| n % multiple == 0 }
-        sum + n
-      else
-        sum
-      end
-    end
-
-    p result
+    self.class.to(limit, @set)
   end
 end
