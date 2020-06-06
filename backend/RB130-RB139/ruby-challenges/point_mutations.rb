f81d0a55c5b8151b2b88617c4c6cbeed4f5657f3
Mandy Cheang <mandycheang135@gmail.com>
2020-06-06 14:00:07 -0400

    Refactor point mutation ruby challenge

--- backend/RB130-RB139/ruby-challenges/point_mutations.rb
+++ backend/RB130-RB139/ruby-challenges/point_mutations.rb
@@ -13,25 +13,52 @@ Algo:
 - return result
 =end
 
+# class DNA
+#   def initialize(dna)
+#     @dna = dna
+#   end
+
+#   def hamming_distance(strand)
+#     @dna.size < strand.size ? short = @dna : short = strand
+#     @dna.size > strand.size ? long = @dna : long = strand
+#     short, long = @dna, strand if @dna.size == strand.size
+
+#     result = 0
+#     short.each_char.with_index do |letter, idx|
+#       result += 1 if letter != long[idx]
+#     end
+
+#     result
+#   end
+# end
+
+# class DNA
+#   def initialize(strand1)
+#     @strand1 = strand1
+#   end
+
+#   def hamming_distance(strand2)
+#     short, long = [@strand1, strand2].sort_by(&:size)
+
+#     distance = 0
+#     short.each_char.with_index do |letter, idx|
+#       distance += 1 if letter != long[idx]
+#     end
+
+#     distance
+#   end
+# end
+
 class DNA
-  def initialize(dna)
-    @dna = dna
+  def initialize(strand1)
+    @strand1 = strand1
   end
 
-  def hamming_distance(strand)
-    @dna.size < strand.size ? short = @dna : short = strand
-    @dna.size > strand.size ? long = @dna : long = strand
+  def hamming_distance(strand2)
+    short, long = [@strand1, strand2].sort_by(&:size)
 
-    if @dna.size == strand.size
-      short = @dna
-      long = strand
+    (0...short.size).inject(0) do |distance, idx|
+      short[idx] == long[idx] ? distance : distance + 1
     end
-
-    result = 0
-    short.each_char.with_index do |letter, idx|
-      result += 1 if letter != long[idx]
-    end
-
-    result
   end
 end
