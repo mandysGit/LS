15707eb42fcf7837a42cc42a50e3a5b5521698d6
Mandy Cheang <mandycheang135@gmail.com>
2020-06-10 17:15:17 -0400

    Refactor scrabble score

--- backend/RB130-RB139/ruby-challenges/scrabble_score.rb
+++ backend/RB130-RB139/ruby-challenges/scrabble_score.rb
@@ -1,33 +1,31 @@
 class Scrabble
-  def self.score(word)
-    letters = word ?  word.scan(/[a-z]/i) : [] 
-    letter_scores = {
-      ['a','e','i','o','u','l','n','r','s','t'] => 1,
-      ['d','g'] => 2,
-      ['b','c','m','p'] => 3,
-      ['f','h','v','w','y'] => 4,
-      ['k'] => 5,
-      ['j','x'] => 8,
-      ['q','z'] => 10
+  def initialize(word)
+    @letters = word ? word.scan(/[a-z]/i) : [] 
+    @letter_scores = {
+      %w(a e i o u l n r s t) => 1,
+      %w(d g) => 2,
+      %w(b c m p) => 3,
+      %w(f h v w y) => 4, 
+      %w(k) => 5,
+      %w(j x) => 8,
+      %w(q z) => 10
     }
-
-    letters.reduce(0) { |score, letter| score + self.find_score(letter_scores, letter) }
   end
 
-  def self.find_score(letter_scores, letter)
+  def self.score(word)
+    new(word).score
+  end
+
+  def score
+    @letters.reduce(0) { |score, letter| score + find_score(@letter_scores, letter) }
+  end
+
+  def find_score(letter_scores, letter)
     score = 0
     letter_scores.each_key do |key|
       return score = letter_scores[key] if key.include?(letter.downcase)
     end
     score 
-  end
-
-  def initialize(word)
-    @word = word
-  end
-
-  def score
-    self.class.score(@word)
   end
 end
 
