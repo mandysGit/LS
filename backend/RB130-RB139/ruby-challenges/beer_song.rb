23db9b657b487b672133bcd260b5235ca89f2922
Mandy Cheang <mandycheang135@gmail.com>
2020-06-13 21:05:44 -0400

    Refactor beer song

--- backend/RB130-RB139/ruby-challenges/beer_song.rb
+++ backend/RB130-RB139/ruby-challenges/beer_song.rb
@@ -43,9 +43,14 @@
 # BONUS
 
 class BeerSong
-  def verse(count)
-    count.lyric
-    line_one(count) + line_two(count)
+  def verse(number)
+    begin
+      verse = Object.const_get("Line#{number}")
+    rescue NameError
+      verse = Line
+    end
+
+    verse.new(number).lyric
   end
 
   def verses(start, finish)
@@ -60,32 +65,36 @@ class BeerSong
   def lyrics
     verses(99, 0)
   end
-
-  def line_one(count)
-    "#{count} bottles of beer on the wall, #{count} bottles of beer.\n"
-  end
-
-  def line_two(count)
-    "Take one down and pass it around, #{count - 1} bottles of beer on the wall.\n"
-  end
 end
 
 class Line
+  def initialize(number)
+    @number = number
+  end
+
   def lyric
+    "#{@number} bottles of beer on the wall, #{@number} bottles of beer.\n" \
+    "Take one down and pass it around, #{@number - 1} bottles of beer on the wall.\n"
   end
 end
 
-class lineZero
+class Line0 < Line
   def lyric
+    "No more bottles of beer on the wall, no more bottles of beer.\n" \
+      "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
   end
 end
 
-class lineOne
+class Line1 < Line
   def lyric
+    "1 bottle of beer on the wall, 1 bottle of beer.\n"  
+    "Take it down and pass it around, no more bottles of beer on the wall.\n"
   end
 end
 
-class lineTwo
+class Line2 < Line
   def lyric
+    "2 bottles of beer on the wall, 2 bottles of beer.\n"  
+    "Take one down and pass it around, 1 bottle of beer on the wall.\n"
   end
 end
