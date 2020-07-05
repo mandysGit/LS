46d1db8710c78abcf965913bd99bd0e08b17e57e
Mandy Cheang <mandycheang135@gmail.com>
2020-07-04 21:58:25 -0400

    Update algorithm on palindrome products challenge

--- backend/RB130-RB139/ruby-challenges/palindrome_products.rb
+++ backend/RB130-RB139/ruby-challenges/palindrome_products.rb
@@ -3,36 +3,20 @@ Problem:
 - Palindromic number reads the same both ways. Ex 9009
 - Detect palindrome products in a given range
 
-- Palindromes generate:
-  - use Array#products to generate all factor pairs
-  - use Array#select to select the pairs that are palindroms
-  -  
-
+- generate:
+  - use Array#permutations to generate all factors (@min..@max).permutations
+  - use Array#select to select the pairs that are factors of a palindrome_product
+  - Create an Array Or Hash to hold all `palindrome` Struct, the value and factors
 =end
 
-class Palindrome
-  def initialize(product, factors)
-    @product = product
-    @factors = factors
-  end
-
-  def value
-    @product
-  end
-
-  def factors
-
-  end
-end
-
 class Palindromes
-  def initialize(max_factor:, min_factor:)
+  def initialize(max_factor:, min_factor: 1)
     @max = max_factor
     @min = min_factor
+    @palindromes = []
   end
 
   def generate
-
   end
 
   def largest
