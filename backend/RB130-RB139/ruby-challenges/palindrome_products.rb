cc20a8196c96d25956c7167391d154e947ebc74c
Mandy Cheang <mandycheang135@gmail.com>
2020-07-06 15:05:18 -0400

    Finish Palindrome Product Ruby Challenge

--- backend/RB130-RB139/ruby-challenges/palindrome_products.rb
+++ backend/RB130-RB139/ruby-challenges/palindrome_products.rb
@@ -5,20 +5,20 @@ Problem:
 
 - generate:
   - Initialize a Hash of Palindromes
-    - Use Range (min..max) and Array#product to create an Array of all factors
+    - Use Range (min..max) and Array#repeated_combination(2) to create an Array of all factors
     - Loop over all factors
       - If we multiply the pair of factors and the product is a Palindrome
       - Then add the product as key and pair as value to the Hash
 
   - helper method `Palindrome?`
-    - Given a Integer
+    - Given an Integer
     - Convert it to a String and compare with the reverse String
     - num.to_s == num.to_s.reverse
 
-  - Each Palindromes will have `factors` and `value` as attributes
-  - Sort the list of Palindromes by `value`
-  - Get the largest `value` Palindrome
-  - Get the factors of a Palindrome
+- largest & smallest:
+  - Create a Struct with attributes value and factors
+  - value is palindromes max key
+  - factors is palindromes[max.key]
 =end
 
 class Palindromes
@@ -32,21 +32,17 @@ class Palindromes
 
   def generate
     arr = *min..max
-    arr.product(arr) do |x, y|
-      palindromes[x * y] << x*y if palindrome?(x*y)
+    arr.repeated_combination(2) do |x, y|
+      palindromes[x * y] << [x,y] if palindrome?(x*y)
     end
   end
 
   def largest
-
+    Struct.new(:value, :factors).new(palindromes.keys.max, palindromes[palindromes.keys.max])
   end
-
-  def value
-
-  end
-
-  def factors
-
+  
+  def smallest
+    Struct.new(:value, :factors).new(palindromes.keys.min, palindromes[palindromes.keys.min])
   end
 
   private
