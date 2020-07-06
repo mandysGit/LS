c2a4d3162d16b8ed68ee936146ae51bfa4ef3e90
Mandy Cheang <mandycheang135@gmail.com>
2020-07-05 21:07:14 -0400

    Add helper method to Palindrome Products

--- backend/RB130-RB139/ruby-challenges/palindrome_products.rb
+++ backend/RB130-RB139/ruby-challenges/palindrome_products.rb
@@ -4,19 +4,37 @@ Problem:
 - Detect palindrome products in a given range
 
 - generate:
-  - use Array#permutations to generate all factors (@min..@max).permutations
-  - use Array#select to select the pairs that are factors of a palindrome_product
-  - Create an Array Or Hash to hold all `palindrome` Struct, the value and factors
+  - Initialize a Hash of Palindromes
+    - Use Range (min..max) and Array#product to create an Array of all factors
+    - Loop over all factors
+      - If we multiply the pair of factors and the product is a Palindrome
+      - Then add the product as key and pair as value to the Hash
+
+  - helper method `Palindrome?`
+    - Given a Integer
+    - Convert it to a String and compare with the reverse String
+    - num.to_s == num.to_s.reverse
+
+  - Each Palindromes will have `factors` and `value` as attributes
+  - Sort the list of Palindromes by `value`
+  - Get the largest `value` Palindrome
+  - Get the factors of a Palindrome
 =end
 
 class Palindromes
+  attr_reader :max, :min, :palindromes
+
   def initialize(max_factor:, min_factor: 1)
     @max = max_factor
     @min = min_factor
-    @palindromes = []
+    @palindromes = Hash.new { |hash, key| hash[key] = [] }
   end
 
   def generate
+    arr = *min..max
+    arr.product(arr) do |x, y|
+      palindromes[x * y] << x*y if palindrome?(x*y)
+    end
   end
 
   def largest
@@ -29,5 +47,11 @@ class Palindromes
 
   def factors
 
+  end
+
+  private
+
+  def palindrome?(product)
+    product.to_s == product.to_s.reverse
   end
 end
