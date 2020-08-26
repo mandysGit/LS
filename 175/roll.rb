c0e22734399e9cc30b4dc6f7a151d63fb6402cf0
Mandy Cheang <mandycheang135@gmail.com>
2020-08-26 14:11:24 -0400

    Parsing path and parameters from http request

---
+++ 175/roll.rb
@@ -0,0 +1,36 @@
+require "socket"
+
+def parse_request(request_line)
+  method, path_and_params, http = request_line.split(" ")
+  path, params = path_and_params.split("?")
+
+  params = params.split("&").each_with_object({}) do |pair, hash|
+    key, value = pair.split("=")
+    hash[key] = value
+  end
+
+  [method, path, params]
+end
+
+server = TCPServer.new("localhost", 3003)
+
+loop do
+  client = server.accept
+
+  request_line = client.gets
+  next if !request_line || request_line =~ /favicon/
+  puts request_line
+
+  http_method, path, params = parse_request(request_line)
+
+  rolls = params["rolls"].to_i
+  sides = params["sides"].to_i
+
+  rolls.times do
+    roll = rand(sides) + 1
+    client.puts roll
+  end
+
+  client.close
+end
+
