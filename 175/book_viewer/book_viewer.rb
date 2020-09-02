48e290f861d8adb10f33fb68b71558b0c41d1352
Mandy Cheang <mandycheang135@gmail.com>
2020-09-02 08:44:20 -0400

    Add helper to display chapters and redirecting

--- 175/book_viewer/book_viewer.rb
+++ 175/book_viewer/book_viewer.rb
@@ -2,21 +2,35 @@ require "tilt/erubis"
 require "sinatra"
 require "sinatra/reloader"
 
+before do 
+  @contents = File.readlines("data/toc.txt")
+end
+
 get "/" do
   @title = "The Adventures of Sherlock Holmes"
-  @contents = File.readlines('data/toc.txt')
   erb :home
 end
 
 get "/chapters/:number" do
-  @contents = File.readlines("data/toc.txt")
-
   number = params[:number].to_i
   chapter_name = @contents[number - 1]
-  @title = "Chapter #{number} : #{chapter_name}"
+  redirect "/" unless (1..@contents.size).cover? number
 
+  @title = "Chapter #{number} : #{chapter_name}"
   @chapter = File.read("data/chp#{number}.txt")
 
   erb :chapter
+end
+
+helpers do
+  def in_paragraphs(text)
+    text.split("\n\n")
+        .map { |paragraph| "<p>#{paragraph}</p>" }
+        .join
+  end
+end
+
+not_found do
+  redirect "/"
 end
 
