b9b8d7a6f854645ae82011bf3e3869be2d986267
Mandy Cheang <mandycheang135@gmail.com>
2020-09-04 14:45:40 -0400

    Refactor handling error and fix rubocop

--- 175/todos/todo.rb
+++ 175/todos/todo.rb
@@ -1,6 +1,6 @@
-require "sinatra"
-require "sinatra/reloader"
-require "tilt/erubis"
+require 'sinatra'
+require 'sinatra/reloader'
+require 'tilt/erubis'
 
 configure do
   enable :sessions
@@ -11,24 +11,42 @@ before do
   session[:lists] ||= []
 end
 
-get "/" do
-  redirect "/lists"
+get '/' do
+  redirect '/lists'
 end
 
 # View list of lists
-get "/lists" do
+get '/lists' do
   @lists = session[:lists]
   erb :lists, layout: :layout
 end
 
 # Render the new list form
-get "/lists/new" do
+get '/lists/new' do
   erb :new_list, layout: :layout
 end
 
+# Return an error message if name is invalid.
+# Return nil if name is valid.
+def error_for_list_name(name)
+  if !(1..100).cover? name.size
+    'List name must be between 1 and 100 characters.'
+  elsif session[:lists].any? { |list| list[:name] == name }
+    'List name must be unique.'
+  end
+end
+
 # Create a new list
-post "/lists" do
-  session[:lists] << { name: params[:list_name], todos: [] }
-  session[:success] = "The list has been created."
-  redirect "/lists"
+post '/lists' do
+  list_name = params[:list_name].strip
+  error = error_for_list_name(list_name)
+
+  if error
+    session[:error] = error
+    erb :new_list, layout: :layout
+  else
+    session[:lists] << { name: list_name, todos: [] }
+    session[:success] = 'The list has been created.'
+    redirect '/lists'
+  end
 end
