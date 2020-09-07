81ac5dab07a9f119dc4934b0d37388d344e8e931
Mandy Cheang <mandycheang135@gmail.com>
2020-09-07 19:25:44 -0400

    Create unique identifier for lists

--- 175/todos/todo.rb
+++ 175/todos/todo.rb
@@ -26,13 +26,6 @@ helpers do
     list[:todos].select { |todo| !todo[:completed] }.size
   end
 
-  def sort_lists(lists, &block)
-    complete_lists, incomplete_lists = lists.partition { |list| list_complete?(list) }
-
-    incomplete_lists.each { |list| yield list, lists.index(list) }
-    complete_lists.each { |list| yield list, lists.index(list) }
-  end
-
   def sort_todos(todos, &block)
     complete_todos, incomplete_todos = todos.partition { |todo| todo[:completed] }
 
@@ -40,8 +33,23 @@ helpers do
     complete_todos.each { |todo| yield todo, todos.index(todo) }
   end
 
-  def next_todo_id(todos)
-    max = todos.map { |todo| todo[:id] }.max || 0
+  def sort_lists(lists, &block)
+    complete_lists, incomplete_lists = lists.partition { |list| list_complete?(list) }
+
+    incomplete_lists.each(&block)
+    complete_lists.each(&block)
+  end
+
+  def load_list(id)
+    list = session[:lists].find { |list| list[:id] == id }
+    return list if list
+
+    session[:error] = "The specified list was not found."
+    redirect "/lists"
+  end
+
+  def next_element_id(elements)
+    max = elements.map { |element| element[:id] }.max || 0
     max + 1
   end
 end
@@ -81,32 +89,29 @@ def error_for_todo(name)
   end
 end
 
-def load_list(index)
-  list = session[:lists][index] if index && session[:lists][index]
-  return list if list
-
-  session[:error] = "The specified list was not found."
-  redirect "/lists"
-end
-
 # Create a new list
 post '/lists' do
   list_name = params[:list_name].strip
-  error = error_for_list_name(list_name)
 
+  error = error_for_list_name(list_name)
   if error
     session[:error] = error
     erb :new_list, layout: :layout
   else
-    session[:lists] << { name: list_name, todos: [] }
+    id = next_element_id(session[:lists])
+    session[:lists] << { id: id, name: list_name, todos: [] }
     session[:success] = 'The list has been created.'
     redirect '/lists'
   end
 end
 
+# View a todo list
 get '/lists/:id' do
-  @list_id = params[:id].to_i
-  @list = load_list(@list_id)
+  id = params[:id].to_i
+  @list = load_list(id)
+  @list_name = @list[:name]
+  @list_id = @list[:id]
+  @todos = @list[:todos]
   erb :list, layout: :layout
 end
 
@@ -137,7 +142,7 @@ end
 # Delete a todo list
 post "/lists/:id/destroy" do
   id = params[:id].to_i
-  session[:lists].delete_at(id)
+  session[:lists].reject! { |list| list[:id] == id }
   if env["HTTP_X_REQUESTED_WITH"] == "XMLHttpRequest"
     "/lists"
   else
@@ -157,7 +162,7 @@ post "/lists/:list_id/todos" do
     session[:error] = error
     erb :list, layout: :layout
   else
-    id = next_todo_id(@list[:todos])
+    id = next_element_id(@list[:todos])
     @list[:todos] << { id: id, name: params[:todo], completed: false }
 
     session[:success] = 'The todo was added.'
