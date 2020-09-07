dad8effc247997c5cf3f0c8e8559223f9ee8732f
Mandy Cheang <mandycheang135@gmail.com>
2020-09-07 18:26:49 -0400

    Add unique identifiers to todo items

--- 175/todos/todo.rb
+++ 175/todos/todo.rb
@@ -39,6 +39,11 @@ helpers do
     incomplete_todos.each { |todo| yield todo, todos.index(todo) }
     complete_todos.each { |todo| yield todo, todos.index(todo) }
   end
+
+  def next_todo_id(todos)
+    max = todos.map { |todo| todo[:id] }.max || 0
+    max + 1
+  end
 end
 
 before do
@@ -152,7 +157,9 @@ post "/lists/:list_id/todos" do
     session[:error] = error
     erb :list, layout: :layout
   else
-    @list[:todos] << { name: params[:todo], completed: false }
+    id = next_todo_id(@list[:todos])
+    @list[:todos] << { id: id, name: params[:todo], completed: false }
+
     session[:success] = 'The todo was added.'
     redirect "/lists/#{@list_id}"
   end
@@ -164,7 +171,8 @@ post "/lists/:list_id/todos/:id/destroy" do
   @list = load_list(@list_id)
 
   todo_id = params[:id].to_i
-  @list[:todos].delete_at(todo_id)
+  @list[:todos].reject! { |todo| todo[:id] == todo_id }
+
   if env["HTTP_X_REQUESTED_WITH"] == "XMLHttpRequest"
     status 204
   else
@@ -180,7 +188,9 @@ post "/lists/:list_id/todos/:id" do
 
   todo_id = params[:id].to_i
   is_completed = params[:completed] == "true"
-  @list[:todos][todo_id][:completed] = is_completed
+  todo = @list[:todos].find { |todo| todo[:id] == todo_id }
+  todo[:completed] = is_completed
+
   session[:success] = "The todo has been updated."
   redirect "/lists/#{@list_id}"
 end
