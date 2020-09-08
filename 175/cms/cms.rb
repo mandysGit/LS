47f07951224c3e4687e6ab1cf8a959f7606d6561
Mandy Cheang <mandycheang135@gmail.com>
2020-09-08 15:34:32 -0400

    Add route to view text files

--- 175/cms/cms.rb
+++ 175/cms/cms.rb
@@ -4,6 +4,11 @@ require 'tilt/erubis'
 
 get '/' do
   @files = Dir.new("data").children
-  erb :list
+  erb :index
+end
+
+get '/:file' do
+  headers["Content-Type"] = "text/plain"
+  File.read("data/#{params[:file]}")
 end
 
