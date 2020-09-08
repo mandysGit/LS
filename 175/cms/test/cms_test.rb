405c356dfd045e11a89bcac91551bef5fd121fa6
Mandy Cheang <mandycheang135@gmail.com>
2020-09-08 16:31:37 -0400

    Add tests for / and /:file routes

---
+++ 175/cms/test/cms_test.rb
@@ -0,0 +1,30 @@
+ENV["RACK_ENV"] = "test"
+
+require "minitest/autorun"
+require "rack/test"
+
+require_relative "../cms"
+
+class AppTest < Minitest::Test
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  def test_index
+    get "/"
+    assert_equal 200, last_response.status
+    assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
+    assert_includes last_response.body, "about.txt"
+    assert_includes last_response.body, "changes.txt"
+    assert_includes last_response.body, "history.txt"
+  end
+
+  def test_file
+    get "/history.txt"
+    assert_equal 200, last_response.status
+    assert_equal "text/plain", last_response["Content-Type"]
+    assert_includes last_response.body, "1993 - Yukihiro Matsumoto dreams up Ruby."
+  end
+end
