dd57868f181a1782a0a7804ee3cd1e8ade43bcf6
Mandy Cheang <mandycheang135@gmail.com>
2021-02-08 15:09:29 -0500

    Rot13 exercise

---
+++ 210/3-logic-flow-control/rot13.js
@@ -0,0 +1,60 @@
+/*
+Problem
+- Given a string consisting of alpha and non-alpha chars
+- Rotate each char of the input string by 13 postions later
+- Return to the beginning if you reach the end of the alphabet 
+- Dont modify non-letters
+
+Algo
+- perform a String transformation
+  - Find all occurences of letters a-z, A-Z
+  - On each occurence of the letter, call rotate helper function
+- return the newString
+
+helper function rotate
+- Store the alphabet in an String `ALPHA`
+- convert the letter to lowercase
+    - find the index of the letter in the `ALPHA` String
+    - Add 13 to the index
+    - If the index becomes greater than 25, minus 26 from the index
+    - minus 26 will allow it to go back to the beginning
+- If letter is capital return ALPHA[index] capitalized
+- If letter is lowercase return ALPHA[index]
+*/
+
+const ALPHABET = 'abcdefghijklmnopqrstuvwxyz';
+const OFFSET = 26;
+const ROTATION = 13;
+const LAST_POSITION_OF_ALPHABET = 25;
+
+function rot13(str) {
+  if (typeof str !== 'string') return '';
+  return str.replace(/[a-zA-Z]/g, rotate)
+}
+
+function rotate(letter) {
+  let index = ALPHABET.indexOf(letter.toLowerCase()) + ROTATION;
+  if (index > LAST_POSITION_OF_ALPHABET) index = index - OFFSET;
+
+  if (letter.match(/[A-Z]/)) return ALPHABET[index].toUpperCase();
+  return ALPHABET[index];
+}
+
+// Edge cases
+console.log(rot13('')); // ''
+console.log(rot13());  // ''
+
+// Symbols & Numbers
+console.log(rot13('^&*^*&')); // ^&*^*&
+console.log(rot13('123'));    // 123
+console.log(rot13('1#1$'));   // 1#1$
+
+// Beginning and end letters
+console.log(rot13('A'));
+console.log(rot13('a'));
+console.log(rot13('z'));
+console.log(rot13('Z'));
+
+// Sample tests
+console.log(rot13('Teachers open the door, but you must enter by yourself.'));
+console.log(rot13(rot13('Teachers open the door, but you must enter by yourself.')));
