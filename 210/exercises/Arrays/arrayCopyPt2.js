876e7114d875b440212e871d5e439ceb167b7b70
Mandy Cheang <mandycheang135@gmail.com>
2021-02-16 14:59:03 -0500

    JS210 Array exercises

---
+++ 210/exercises/Arrays/arrayCopyPt2.js
@@ -0,0 +1,9 @@
+// Only valaues of the array are copied
+let myArray = [1, 2, 3, 4];
+const myOtherArray = myArray.slice();
+
+myArray.pop();
+console.log(myOtherArray);
+
+myArray = [1, 2];
+console.log(myOtherArray);
