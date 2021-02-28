69593856596a1ac8a1b9264b3a4086e2403171c0
Mandy Cheang <mandycheang135@gmail.com>
2021-02-27 20:13:39 -0500

    Finished Array Exercises

---
+++ 210/exercises/Arrays/areArraysEqual.js
@@ -0,0 +1,22 @@
+function areArraysEqual(array1, array2) {
+  if (array1.length !== array2.length) return false;
+  sorted1 = array1.slice().sort();
+  sorted2 = array2.slice().sort();
+
+  for (let i = 0; i < sorted1.length; i++) {
+    if (sorted1[i] !== sorted2[i]) return false;
+  };
+
+  return true;
+}
+
+console.log(areArraysEqual([1, 2, 3], [1, 2, 3]));                  // true
+console.log(areArraysEqual([1, 2, 3], [3, 2, 1]));                  // true
+console.log(areArraysEqual(['a', 'b', 'c'], ['b', 'c', 'a']));      // true
+console.log(areArraysEqual(['1', 2, 3], [1, 2, 3]));                // false
+console.log(areArraysEqual([1, 1, 2, 3], [3, 1, 2, 1]));            // true
+console.log(areArraysEqual([1, 2, 3, 4], [1, 1, 2, 3]));            // false
+console.log(areArraysEqual([1, 1, 2, 2], [4, 2, 3, 1]));            // false
+console.log(areArraysEqual([1, 1, 2], [1, 2, 2]));                  // false
+console.log(areArraysEqual([1, 1, 1], [1, 1]));                     // false
+console.log(areArraysEqual([1, 1], [1, 1]));                        // true
