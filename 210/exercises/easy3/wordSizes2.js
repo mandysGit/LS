4eb91fb13c3d327f8f1634fa479a0b41c09d362e
Mandy Cheang <mandycheang135@gmail.com>
2021-03-07 16:28:08 -0500

    refactor easy 3 wordsizes

--- 210/exercises/easy3/wordSizes2.js
+++ 210/exercises/easy3/wordSizes2.js
@@ -18,8 +18,7 @@ function size(word) {
   let removeSymbols = word.replace(/[^a-z]/ig, '');
   return removeSymbols.length;
 }
-console.log(wordSizes('Four score and seven.'));                       // { "3": 1, "4": 1, "5": 1, "6": 1 }
-console.log(wordSizes('Hey diddle diddle, the cat and the fiddle!'));  // { "3": 5, "6": 1, "7": 2 }
-console.log(wordSizes("What's up doc?"));                              // { "2": 1, "4": 1, "6": 1 }
+console.log(wordSizes('Four score and seven.'));                       // { "3": 1, "4": 1, "5": 2 }
+console.log(wordSizes('Hey diddle diddle, the cat and the fiddle!'));  // { "3": 5, "6": 3 }
+console.log(wordSizes("What's up doc?"));                              // { "5": 1, "2": 1, "3": 1 }
 console.log(wordSizes(''));                                            // {}
-
